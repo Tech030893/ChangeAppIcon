f73c487605cc573e6e00e3b7bee45b436ebf1e72
Tech030893 <gauravsgujar1993@gmail.com>
2021-05-28 21:08:53 +0530

    UI changes

--- ChangeAppIcon/ViewController.swift
+++ ChangeAppIcon/ViewController.swift
@@ -2,9 +2,16 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var carBtn: UIButton!
+    @IBOutlet weak var bikeBtn: UIButton!
+    @IBOutlet weak var truckBtn: UIButton!
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        carBtn.layer.cornerRadius = 12
+        bikeBtn.layer.cornerRadius = 12
+        truckBtn.layer.cornerRadius = 12
     }
     
     @IBAction func carPress(_ sender: UIButton)
